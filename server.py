ff4677a9ebf50b30c21d983a681e93ada48e1683
Stremyguy <stremyguy@gmail.com>
2025-05-05 12:23:50 +0300

    Update of the guessing a city game

--- server.py
+++ server.py
@@ -16,18 +16,23 @@ cities = {
 sessionStorage = {}
 
 
-@app.route("/post", methods=["POST"])
+@app.route("/", methods=["GET", "POST"])
 def main() -> dict:
+    if request.method == "GET":
+        return "Server is running!", 200
+    
     logging.info(f"Request: {request.json!r}")
+    
     response = {
         "session": request.json["session"],
         "version": request.json["version"],
         "response": {
-            "end_session": False
+            "end_session": False,
+            "text": ""
         }
     }
     handle_dialogue(response, request.json)
-    logging.info(f"Response: {response!r}")
+    logging.info(f"Response: %r", response)
     return jsonify(response)
 
 
@@ -37,7 +42,8 @@ def handle_dialogue(res: dict, req: dict) -> None:
     if req["session"]["new"]:
         res["response"]["text"] = "Привет! Назови свое имя!"
         sessionStorage[user_id] = {
-            "first_name": None
+            "first_name": None,
+            "game_started": False
         }
         return
     
@@ -49,27 +55,71 @@ def handle_dialogue(res: dict, req: dict) -> None:
                 "Не расслышала имя. Повтори, пожалуйста!"
         else:
             sessionStorage[user_id]["first_name"] = first_name
-            res["response"][
-                "text"] = "Приятно познакомиться, " \
-                    + first_name.title() \
-                    + ". Я - Алиса. Какой город хочешь увидеть?"
+            sessionStorage[user_id]["guessed_cities"] = []
+            res["response"]["text"] = f"Приятно познакомиться, {first_name.title()}. Я - Алиса. Отгадаешь город по фото?"
             res["response"]["buttons"] = [
-                {
-                    "title": city.title(),
-                    "hide": True
-                } for city in cities
+                {"title": "Да", "hide": True},
+                {"title": "Нет", "hide": True}
             ]
     else:
-        city = get_city(req)
-        
-        if city in cities:
-            res["response"]["card"] = {}
-            res["response"]["card"]["type"] = "BigImage"
-            res["response"]["card"]["title"] = "Этот город я знаю."
-            res["response"]["card"]["image_id"] = random.choice(cities[city])
+        if not sessionStorage[user_id]["game_started"]:
+            if "да" in req["request"]["nlu"]["tokens"]:
+                if len(sessionStorage[user_id]["guessed_cities"]) == 3:
+                    res["response"]["text"] = "Ты отгадал все города!"
+                    res["end_session"] = True
+                else:
+                    sessionStorage[user_id]["game_started"] = True
+                    sessionStorage[user_id]["attempt"] = 1
+                    play_game(res, req)
+            elif "нет" in req["request"]["nlu"]["tokens"]:
+                res["response"]["text"] = "Ну и ладно!"
+                res["end_session"] = True
+            else:
+                res["response"]["text"] = "Не поняла ответа! Так да или нет?"
+                res["response"]["buttons"] = [
+                    {"title": "Да", "hide": True},
+                    {"title": "Нет", "hide": True},
+                ]
         else:
-            res["response"]["text"] = \
-                "Первый раз слышу об этом городе. Попробуй еще разок!"
+            play_game(res, req)
+
+
+def play_game(res: dict, req: dict) -> None:
+    user_id = req["session"]["user_id"]
+    attempt = sessionStorage[user_id]["attempt"]
+    
+    if attempt == 1:
+        city = random.choice(list(cities))
+        while city in sessionStorage[user_id]["guessed_cities"]:
+            city = random.choice(list(cities))
+        sessionStorage[user_id]["city"] = city
+        res["response"]["card"] = {
+            "type": "BigImage",
+            "title": "Что это за город?",
+            "image_id": cities[city][attempt - 1]
+        }
+        res["response"]["text"] = "Тогда сыграем!"
+    else:
+        city = sessionStorage[user_id]["city"]
+        if get_city(req) == city:
+            res["response"]["text"] = "Правильно! Сыграем ещё?"
+            sessionStorage[user_id]["guessed_cities"].append(city)
+            sessionStorage[user_id]["game_started"] = False
+            return
+        else:
+            if attempt == 3:
+                res["response"]["text"] = f"Вы пытались. Это {city.title()}. Сыграем ещё?"
+                sessionStorage[user_id]["game_started"] = False
+                sessionStorage[user_id]["guessed_cities"].append(city)
+                return
+            else:
+                res["response"]["card"] = {
+                    "type": "BigImage",
+                    "title": "Неправильно. Вот тебе дополнительное фото",
+                    "image_id": cities[city][attempt - 1]
+                }
+                res["response"]["text"] = "А вот и не угадал!"
+    sessionStorage[user_id]["attempt"] += 1
 
 
 def get_city(req: dict) -> Optional[str]:
